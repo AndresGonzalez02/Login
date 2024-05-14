a9a979261a4ae91bb1f396b2a5de835a8646a160
AndresGonzalez02 <144648437+AndresGonzalez02@users.noreply.github.com>
2024-05-14 12:30:29 -0500

    prueba

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -20,7 +20,7 @@ if (loginin) {
         await displayUserData();
     }
     else{
-        alert('Error authentication no sucessfull ')
+        alert('Error authentication no sucessfullll ')
         console.log('sesion '+email+' no validation')
     }
     
